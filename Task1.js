9b6c190cffe77e7c7a0beee94cca12d23cb8e39e
Valarmathi G <141111982+Valarmathi8@users.noreply.github.com>
2023-11-08 10:26:02 +0530

    Object Task

--- Task1.js
+++ Task1.js
@@ -1,5 +1,8 @@
-// task1
-let person=(x)=>{
-    console.log(x);
+var data={
+    name: 'Mercedes Benz',
+    model: 'C-Class',
+    manufacturer: 'Daimler AG company',
+    length: '4686mm',
+    cargocapacity: '12.6 cubic feet' 
 }
-person("Hello World");
+console.log(data)
