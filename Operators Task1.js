a4add2d4401e9e29564ad4ebc711c9c9200926b3
Valarmathi G <141111982+Valarmathi8@users.noreply.github.com>
2023-11-08 10:26:43 +0530

    Operators Task

---
+++ Operators Task1.js
@@ -0,0 +1,25 @@
+function q1(){
+    var a=5,b=7,c=a*b;
+    alert(c);
+}
+q1();
+
+function q2(){
+    var a=5,b=7,c=a%b;
+    alert(c);
+}
+q2();
+
+function q3(){
+    var x=10,y=10,z=x+y;
+    console.log(z);
+}
+q3();
+
+function q4(){
+    var x=20,y=10,z=x+y;
+    console.log(z);
+}
+q4();
+
+
